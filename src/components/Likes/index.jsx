e0a5788913019d30e40ffee0b67087e60c832036
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-06-03 11:33:43 +0200

    ukol c. 14 - pridani funkce na likes

--- src/components/Likes/index.jsx
+++ src/components/Likes/index.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export const Likes = () => {
+  const [likeUp, setLike] = useState(0);
+  const [likeDown, setLikeDown] = useState(0);
+
+  const likeUpFunc = () => {
+    setLike(likeUp + 1);
+  };
+
+  const likeDownFunc = () => {
+    setLikeDown(likeDown + 1);
+  };
+
   return (
     <div className="joke__likes">
-      <button id="btn-up" className="btn-like btn-like--up"></button>
+      <button
+        id="btn-up"
+        className="btn-like btn-like--up"
+        onClick={likeUpFunc}
+      ></button>
       <span id="likes-up" className="likes-count likes-count--up">
-        0
+        {likeUp}
       </span>
-      <button id="btn-down" className="btn-like btn-like--down"></button>
+      <button
+        id="btn-down"
+        className="btn-like btn-like--down"
+        onClick={likeDownFunc}
+      ></button>
       <span id="likes-down" className="likes-count likes-count--down">
-        0
+        {likeDown}
       </span>
     </div>
   );
